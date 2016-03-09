63a7fb514829558ac2ce2c3eaaface6310a2f0e9
Caamy <miss.Caamy@gmail.com>
2016-03-09 14:50:20 +0100

    bowling.java

--- src/main/java/dcll/projet/Bowling.java
+++ src/main/java/dcll/projet/Bowling.java
@@ -4,7 +4,13 @@ package dcll.projet;
  * Created by Camy on 09/03/2016.
  */
 public class Bowling {
-    
 
 
+    private int score;  // score du type qui joue et qu'est pas forcément très fort
+
+    public Bowling() {
+        score = 0;
+    }
+
+    public void estValide(){}
 }
