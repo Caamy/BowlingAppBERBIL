d15061d58fd857d6e7716f9b09e90c596db13687
Caamy <miss.Caamy@gmail.com>
2016-03-09 14:17:52 +0100

    initial commit

---
+++ src/main/java/dcll/projet/App.java
@@ -0,0 +1,13 @@
+package dcll.projet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
